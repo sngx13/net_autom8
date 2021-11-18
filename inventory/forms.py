1a530f1f07434de8377bba9d18fa19638ccb74a7
sngx13 <serghei.golipad@gmail.com>
2021-11-18 15:53:50 +0000

    added Django Crisp Forms

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,20 +1,45 @@
 from django import forms
 from django.core.validators import FileExtensionValidator
+from django.utils.html import mark_safe
 
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(
-        label='Select a file',
+        label='',
+        widget=forms.FileInput(
+            attrs={'class': 'form-control form-control-sm'}
+        ),
         required=False,
         validators=[FileExtensionValidator(allowed_extensions=['csv'])]
     )
 
 
 class DeviceCreateForm(forms.Form):
-    hostname = forms.CharField()
-    mgmt_ip = forms.CharField()
-    software_version = forms.CharField()
-    serial_number = forms.CharField()
-    vendor = forms.CharField()
-    hardware_model = forms.CharField()
-    location = forms.CharField()
+    hostname = forms.CharField(
+        label= mark_safe('<i class="fas fa-signature"></i> Hostname'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
+    mgmt_ip = forms.CharField(
+        label= mark_safe('<i class="fas fa-at"></i> MGMT IP'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
+    software_version = forms.CharField(
+        label= mark_safe('<i class="fas fa-code-branch"></i> Software'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
+    serial_number = forms.CharField(
+        label= mark_safe('<i class="fas fa-barcode"></i> Serial'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
+    vendor = forms.CharField(
+        label= mark_safe('<i class="fas fa-briefcase"></i> Vendor'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
+    hardware_model = forms.CharField(
+        label= mark_safe('<i class="fas fa-sitemap"></i> Model'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
+    location = forms.CharField(
+        label= mark_safe('<i class="fas fa-search-location"></i> Location'),
+        widget=forms.TextInput(attrs={'class': 'form-control form-control-sm'})
+    )
