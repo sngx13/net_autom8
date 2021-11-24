554dc1c1253a8d4019dd8e356cd705506a3537b9
sngx13 <serghei.golipad@gmail.com>
2021-11-24 14:59:18 +0000

    fixed housekeeping views

--- housekeeping/views.py
+++ housekeeping/views.py
@@ -1,24 +1,39 @@
 from django.shortcuts import render
 from celery import current_app
+from celery.utils.nodenames import gethostname
 from .models import CeleryJobResults
 
 
 def housekeeping_celery_tasks(request):
-    tasks = current_app.control.inspect(['celery@django'])
+    hostname = 'celery@' + str(gethostname())
+    tasks = current_app.control.inspect([hostname])
+    tasks_list = CeleryJobResults.objects.all().order_by('-start_time')
     context = {
-        'title': 'Housekeeping - Celery Tasks',
-        'card_header': 'Housekeeping - Celery Tasks',
-        'data': {
-            'celery_statistics': tasks.stats(),
-            'registered_tasks': tasks.registered_tasks(),
-            'scheduled_tasks': tasks.scheduled(),
-            'active_tasks': tasks.active(),
-            'reserved_tasks': tasks.reserved(),
-            'tasks_list': CeleryJobResults.objects.all()
+            'title': 'Housekeeping - Celery Tasks',
+            'card_header': 'Housekeeping - Celery Tasks',
+            'data': {
+                'tasks_list': tasks_list
+            }
         }
-    }
-    return render(
-        request,
-        'housekeeping/housekeeping_celery_tasks.html',
-        context
-    )
+    if tasks.stats():
+        data = {
+            'celery_information': {
+                'celery_statistics': tasks.stats(),
+                'registered_tasks': tasks.registered_tasks()[hostname],
+                'scheduled_tasks': tasks.scheduled()[hostname],
+                'active_tasks': tasks.active()[hostname],
+                'reserved_tasks': tasks.reserved()
+            }
+        }
+        context['data'].update(data)
+        return render(
+            request,
+            'housekeeping/housekeeping_celery_tasks.html',
+            context
+        )
+    else:
+        return render(
+                request,
+                'housekeeping/housekeeping_celery_tasks.html',
+                context
+            )
