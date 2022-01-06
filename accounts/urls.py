19b29e7a0db9329028da815a006c85407df2c58e
sngx13 <serghei.golipad@gmail.com>
2022-01-06 15:42:46 +0000

    added visitors to logs page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login_history/', views.login_history, name='login_history')
+    path('login_history/', views.login_history, name='login_history'),
+    path('get_today_visitors/', views.get_today_visitors, name='get_today_visitors')
 ]
